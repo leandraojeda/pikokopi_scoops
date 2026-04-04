eabc427bdc48d3c1d50d8d5542d61166b3278e8f
unknown <leandra.ojeda67@gmail.com>
2026-04-04 13:10:49 -0400

    olaasdasdasda

--- app.js
+++ app.js
@@ -385,6 +385,7 @@ app.get('/descargar-recibo/:nro', (req, res) => {
 
 // --- INICIAR SERVIDOR ---
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Piko Kopi funcionando en puerto ${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
